40dab1a0c1c93e6bc3b0f33ac5b7902932399dfc
matuzaite <matuzaiteausteja@gmail.com>
2025-11-29 12:19:39 +0100

    Fix Prisma 7 config + client

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.POSTGRES_PRISMA_URL!,
   },
 });
